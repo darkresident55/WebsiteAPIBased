1426200bf3b1356e94b19bb52fd112e13ce5d9ed
Anemunt <69436164+darkresident55@users.noreply.github.com>
2025-04-14 16:43:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 @app.route('/privacyandsafety')
-def home():
+def safetyscreen():
     return render_template('Privacy-and-Safety-Finished.html')
 
 if __name__ == '__main__':
